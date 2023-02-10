c34b08fb1fca105c73f81a75fbb543128f620c57
Benjamin Fredette <98136630+BenFredette@users.noreply.github.com>
2023-02-10 14:12:07 -0500

    Add files via upload

---
+++ Lab2/src/main/java/ryerson/ca/lab2/Appointment.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ryerson.ca.lab2;
+
+/**
+ *
+ * @author student
+ */
+public class Appointment {
+    Doctor doctor;
+    Patient patient;
+    int time;
+    String day;
+}
