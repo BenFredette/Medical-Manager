658ba7a081d32560b8232d3fe2d558787e395e02
Benjamin Fredette <98136630+BenFredette@users.noreply.github.com>
2023-03-27 00:36:38 -0400

    Add files via upload

---
+++ lab4/lab4/Prescribe/src/main/java/helper/Prescription_XML.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helper;
+
+/**
+ *
+ * @author student
+ */
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+import helper.Prescription;
+
+ @XmlRootElement(name = "prescriptions")
+@XmlAccessorType (XmlAccessType.FIELD)
+       public class Prescription_XML{
+     @XmlElement(name="prescription")
+           private ArrayList<Prescription> prescriptions;
+           
+           
+           public List<Prescription>getPrescription(){
+               return prescriptions;
+               
+           }
+          public  Prescription_XML(){
+               
+               
+           }
+           public void setPrescription(ArrayList<Prescription> pr){
+               prescriptions=pr;
+               
+           }
+           
+       }
